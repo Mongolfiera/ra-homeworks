e9ee9787220e306394eac3fa4467bc1d7e8dd0ca
Mongolfiera <elenadruganova@mail.ru>
2018-10-23 23:34:40 +0300

    Update Stars.js

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,13 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count = 0}) {
+  if (count >= 1 && count <= 5 && typeof count === 'number') {
+    return (
+      <ul className="card-body-stars u-clearfix">
+        <li>
+          {new Array(count).fill(<Star />)}
+        </li>
+      </ul>
+    );
+  }
 }
