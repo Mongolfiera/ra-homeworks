bf27f2b1c5ccd7fa0cc6816b100644c8c9414daa
Mongolfiera <elenadruganova@mail.ru>
2018-10-24 08:42:45 +0300

    Update MessageHistory.js

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const MessageHistory = ({list = []}) => {
+  if (!list.length) {
+    return <ul />;
+  }
+  return (
+    <ul>
+      { list.map((m) => {
+         switch (m.type) {
+           case 'response':
+             return ( <Response key = {m.id} from = {m.from} message = {m} /> );
+           case 'message': 
+             return ( <Message key = {m.id} from = {m.from} message = {m} /> );
+           case 'typing': 
+             return ( <Typing key = {m.id} from = {m.from} message = {m} /> );
+         }
+       }
+      )}
+    </ul> 
+  );
+}
