f15af33fe0cc8be6a05b597d2d02b9cd894917b2
Mongolfiera <elenadruganova@mail.ru>
2018-10-27 08:20:47 +0300

    Update app.js

--- func-component/listing/js/app.js
+++ func-component/listing/js/app.js
@@ -1,1 +1,55 @@
 'use strict';
+
+fetch(' https://neto-api.herokuapp.com/etsy')
+  .then((response) => {
+    if (200 <= response.status && response.status < 300) {
+      return response;
+    }
+    throw new Error(response.statusText);
+  })
+  .then((response) => response.json())
+  .then((items) => { ReactDOM.render(<Listing items={items} />, document.getElementById('root')) })
+  .catch((error) => { console.log(error)});
+
+const cutOff = str => {
+  return str.slice(0, 49).concat(str.slice(50) && '...');
+}
+
+const highlightLevel = quantity => {
+  return (
+    quantity <= 10 && 'level-low' || quantity <= 20 && 'level-medium' || 'level-high'
+  );
+}
+
+const showCurrency = (price, currency) => {
+  const formatCurrency = (num, currency) => {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(num);
+  }
+  return (currency === 'USD' ||  currency === 'EUR') && formatCurrency(price, currency) || `${price} ${currency}`;
+}
+
+const Listing = ({items}) => {
+  let item = items[0]
+  console.log(item);
+
+  return (
+  <div className="item-list">
+    { items.map( item => { 
+      return (
+        <div key = { item.listing_id } className="item">
+          <div className="item-image">
+            <a href={item.url}>
+              <img src={item.MainImage.url_570xN} />
+            </a>
+          </div>
+          <div className="item-details">
+            <p className="item-title">{cutOff(item.title)}</p>
+            <p className="item-price">{showCurrency(item.price, item.currency_code)}</p>
+            <p className={'item-quantity '.concat(highlightLevel(item.quantity))}>{item.quantity} left</p>
+          </div>
+        </div>   
+      );}
+    ) }
+  </div>
+  );
+}
