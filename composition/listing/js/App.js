103653da642c92a9b41a27a5c2849292c4000caf
Mongolfiera <elenadruganova@mail.ru>
2018-11-02 08:29:02 +0300

    Update App.js

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const App = ({items}) => (
-  <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
-  </main>
+const colorScheme = {
+  'unisex': 'black',
+  'male': 'blue',
+  'female': 'orange'
+}
+
+const getItems = ({items}) => items.map(item => 
+  <Item color={colorScheme[item.type]} item={item} />
 );
+
+const App = ({items}) => {console.log(items); return(
+  <main>
+    {getItems({items})}
+  </main>
+)};
