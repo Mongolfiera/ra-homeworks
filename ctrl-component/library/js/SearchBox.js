ac432b2a5bbdc553d4b6575d70afac1f540036a5
Mongolfiera <elenadruganova@mail.ru>
2018-11-15 08:50:34 +0300

    Update SearchBox.js

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,9 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
+const SearchBox = (props) => {
+  return (
+    <input 
+      type="text" 
+      placeholder="Поиск по названию или автору"
+      onChange={event => props.filterBooks(event.currentTarget.value)}
+    />
+  );
 };
