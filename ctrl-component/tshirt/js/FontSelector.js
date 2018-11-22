cdb850fea3508673f046de67e8ac9ac75e91101d
Mongolfiera <elenadruganova@mail.ru>
2018-11-22 12:47:37 +0300

    Update FontSelector.js

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,25 @@
 const FontSelector = ({fonts, selectedFont, onSelect}) => {
-    return (
-        <div className="font-picker">
-            Выберите шрифт
+ 
+  const handleSelect = event => {
+    selectedFont = fonts.filter(font => font.name === event.target.value)[0];
+    onSelect(selectedFont);
+  }
+
+  return (
+    <div className="font-picker">
+      
+    {fonts.map((font, i) => {
+      return (
+        <div className="grid center font-item">
+          <input type="radio" name="font" value={font.name} id={font.name} onChange={handleSelect}/>
+          <label htmlFor={font.name} class="grid-1">
+            <PictureFont text={'abc'} path={font.path}/>
+          </label>
         </div>
-    )
+        );
+      })}
+
+    </div>
+  );
+
 };
