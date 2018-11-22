bd67672742572277d01231a7f1b01770f4caf32e
Mongolfiera <elenadruganova@mail.ru>
2018-11-22 12:49:16 +0300

    Update TextRenderLine.js

--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,13 @@
 const TextRenderLine = ({value, onChange}) => {
-	return (
-		<div className="type-text">
-			Наберите текст
-		</div>
-	);
+ 
+  const handleChange = event => {
+    event.target.value = event.target.value.replace(/[^a-z\s]/gi, '');    
+    onChange(event.target.value.toLowerCase());
+  }
+  
+  return (
+    <div class="type-text">
+      <textarea name="text" id="font-text" cols="30" rows="2" placeholder="Введите текст для футболки" onChange={handleChange} value={value} />
+    </div>
+  );
 };
